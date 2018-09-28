b8a35eb9d4103bdb5b6504229f1a6b0be8b724f6
Jasper den Hertog <jdenhertog@ripe.net>
2018-09-28 11:10:38 +0200

    working ws (slow loading)

--- main.rs
+++ main.rs
@@ -1,16 +1,22 @@
 #[macro_use]
 extern crate serde_derive;
 
-extern crate csv;
+extern crate actix;
+extern crate actix_web;
+extern crate flate2;
 extern crate serde;
 
+use std::collections::BTreeMap;
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
 use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 use std::process;
 
-use std::collections::HashMap;
+use actix_web::{http, server, App, HttpRequest, HttpResponse, Path, State};
+use flate2::read::GzDecoder;
 
 #[derive(Debug, Deserialize)]
 struct SimilarityRecord {
@@ -25,9 +31,18 @@ struct SimilarityRecord {
     pct75_similarity: f32,
 }
 
-fn run() -> Result<(), Box<Error>> {
-    let mut similarity_map = HashMap::new();
+struct AppState {
+    similarity_map: BTreeMap<u32, Vec<(u32, f32)>>,
+}
 
+#[derive(Deserialize)]
+struct Info {
+    prb_id: String,
+}
+
+fn run<'a>(
+    mut similarity_map: BTreeMap<u32, Vec<(u32, f32)>>,
+) -> Result<BTreeMap<u32, Vec<(u32, f32)>>, Box<Error>> {
     let prb_id = get_second_arg()?
         .into_string()
         .unwrap()
@@ -35,25 +50,30 @@ fn run() -> Result<(), Box<Error>> {
         .unwrap();
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(false)
-        .delimiter(b' ')
-        // .double_quote(false)
-        // .escape(Some(b'\\'))
-        // .flexible(true)
-        // .comment(Some(b'#'))
-        .from_reader(file);
-    for result in rdr.deserialize() {
-        let record: SimilarityRecord = result?;
-        let prb_entry = similarity_map.entry(record.prb_id1).or_insert(vec![]);
-        prb_entry.push((record.prb_id2, record.pct50_similarity));
 
+    let data = GzDecoder::new(file);
+
+    for line in io::BufReader::new(data).lines().skip(1) {
+        let line = line.unwrap();
+        let b: Vec<String> = line.split_whitespace().map(|s| s.to_string()).collect();
+        let prb_id1 = b[0].parse::<u32>()?;
+        let prb_id2 = b[1].parse::<u32>()?;
+        let pct50_similarity = b[7].parse::<f32>()?;
+        let prb_entry = similarity_map.entry(prb_id1).or_insert(vec![]);
+        prb_entry.push((prb_id2, pct50_similarity));
     }
 
-    let simils: Vec<&(u32, f32)> = similarity_map.get(&prb_id).unwrap().into_iter().filter(|ps| ps.1 > 0.5).collect();
+    println!("Done building state. now searching");
+    let simils: Vec<&(u32, f32)> = similarity_map
+        .get(&prb_id)
+        .unwrap()
+        .into_iter()
+        .filter(|ps| ps.1 > 0.5)
+        .collect();
     println!("{:?}", simils.len());
     println!("{:?}", simils);
-    Ok(())
+
+    Ok(similarity_map)
 }
 
 fn get_first_arg() -> Result<OsString, Box<Error>> {
@@ -69,10 +89,35 @@ fn get_second_arg() -> Result<OsString, Box<Error>> {
         Some(prb_id) => Ok(prb_id),
     }
 }
+fn index(data: (State<AppState>, Path<Info>)) -> String {
+    // fn index(req: &HttpRequest<AppState>) -> HttpResponse {
+    let (state, path) = data;
+    let prb_id: u32 = path.prb_id.parse::<u32>().unwrap();
+    let ss: Vec<&(u32, f32)> = state
+        .similarity_map
+        .get(&prb_id)
+        .unwrap()
+        .into_iter()
+        .filter(|ps| ps.1 > 0.5)
+        .collect();
+    format!("{:?}", ss)
+}
 
 fn main() {
-    if let Err(err) = run() {
-        println!("{}", err);
-        process::exit(1);
-    }
+    let sys = actix::System::new("probe-similarity");
+
+    server::new(|| {
+        let similarity_map: BTreeMap<u32, Vec<(u32, f32)>> = BTreeMap::new();
+        let similarity_map = run(similarity_map).unwrap();
+
+        App::with_state(AppState {
+            similarity_map: similarity_map,
+        }).prefix("/probe-similarity")
+        .resource("/{prb_id}", |r| r.method(http::Method::GET).with(index))
+    }).bind("127.0.0.1:8100")
+    .unwrap()
+    .start();
+
+    println!("Started http server: 127.0.0.1:8100");
+    let _ = sys.run();
 }
